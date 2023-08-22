82575c207e8fe6d0fb88c1fe7c706ba54ce011a8
Anais Bernard <anais.mauricette.bernard@gmail.com>
2023-08-22 17:24:46 +0200

    Toujours def new

--- app/controllers/lives_controller.rb
+++ app/controllers/lives_controller.rb
@@ -15,7 +15,12 @@ class LivesController < ApplicationController
 
   def create
     @life = Life.new(life_params)
-    @life.picture_url = params[:life][:picture_url].path
+
+    if params[:life][:picture_url].present?
+      cloudinary_response = Cloudinary::Uploader.upload(params[:life][:picture_url].tempfile.path)
+      @life.picture_url = cloudinary_response['secure_url']
+    end
+
     if @life.save
       redirect_to life_path(@life)
     else
@@ -23,12 +28,13 @@ class LivesController < ApplicationController
     end
   end
 
+
   private
 
   def set_life
     @life = Life.find(params[:id])
   end
-  
+
   def life_params
     params.require(:life).permit(:name, :price_per_day, :description, :picture_url, :city, :address, :owner_id)
   end
