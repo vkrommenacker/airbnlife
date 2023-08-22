5bf04c31946bc8fb83fd49b5c1fad190461637f5
emilien le borgne <emilien_leborgne@icloud.com>
2023-08-22 16:56:52 +0200

    show + show view

--- app/controllers/lives_controller.rb
+++ app/controllers/lives_controller.rb
@@ -1,10 +1,11 @@
 class LivesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
-
+  before_action :set_life, only: [ :show ]
   def index
   end
 
   def show
+
   end
 
   def new
@@ -13,4 +14,9 @@ class LivesController < ApplicationController
   def create
   end
 
+  private
+
+  def set_life
+    @life = Life.find(params[:id])
+  end
 end
